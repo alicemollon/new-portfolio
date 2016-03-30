a2833843adf140f39620d315ec60f67c45fdcb19
Alice Mollon <alicemollon@pc104.home>
2016-03-30 21:54:37 +0200

    dot loaders added

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// $(window).load(function() {
-// 		var $grid = $('.grid').imagesLoaded( function() {
-
-// 		$('.grid').isotope({
-// 		columnWidth: '.grid-sizer',
-// 		itemSelector: '.item',
-// 		percentPosition: true,
-// 		isAnimated: !Modernizr.csstransitions
-// 	});
-// });
-
 $(document).ready(function() {
 
 	var imgCount = 0;
@@ -21,7 +10,7 @@ $(document).ready(function() {
 		isAnimated: !Modernizr.csstransitions
 	});
 		
-		$('.progress').fadeOut(1000, function() {
+		$('.loader').delay(2000).fadeOut(900, function() {
 			is.isotope('layout');
 			is.fadeTo(2000, 1);
 		});
