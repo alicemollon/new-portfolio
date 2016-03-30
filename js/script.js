85779c7f69745f737a88473b19020982a514b08d
Alice Mollon <alicemollon@pc104.home>
2016-03-30 13:24:55 +0200

    add pre-loader

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // $(window).load(function() {
+// 		var $grid = $('.grid').imagesLoaded( function() {
 
 // 		$('.grid').isotope({
 // 		columnWidth: '.grid-sizer',
@@ -6,17 +7,25 @@
 // 		percentPosition: true,
 // 		isAnimated: !Modernizr.csstransitions
 // 	});
+// });
 
-$(window).load(function() {
-		var $grid = $('.grid').imagesLoaded( function() {
+$(document).ready(function() {
 
+	var imgCount = 0;
+	var noOfImages = $('.grid img').length;
+	var is = $('.grid').imagesLoaded().always(function() {
 		$('.grid').isotope({
 		columnWidth: '.grid-sizer',
 		itemSelector: '.item',
 		percentPosition: true,
 		isAnimated: !Modernizr.csstransitions
 	});
-});
+		
+		$('.progress').fadeOut(1000, function() {
+			is.isotope('layout');
+			is.fadeTo(2000, 1);
+		});
+	})
 
 
 $('.trigger').on('click', function() {
@@ -24,3 +33,6 @@ $('.trigger').on('click', function() {
 	return false;
 	});
 });
+
+
+
